7d97de01442766339de31892b7c1d1c77b510601
Roman Hoog Antink <rha@open.ch>
2023-04-19 16:46:54 +0200

    Migrate rangeGlitch1 to mono repo

---
+++ rangeGlitch1/go.mod
@@ -0,0 +1,3 @@
+module Challenges/rangeGlitch1
+
+go 1.18
---
+++ rangeGlitch1/main.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+/*
+	Run this code multiple times and compare the results.
+
+	Side note: also compare running this with go run main.go and a compiled binary:
+		1. i=0; while [ $i -lt 100 ]; do go run main.go; let i++; done
+		2. go build; i=0; while [ $i -lt 100 ]; do ./rangeGlitch1; let i++; done
+*/
+
+func isEvenNumber(n int) bool {
+	return n%2 == 0
+}
+
+func main() {
+	ch := make(chan bool)
+
+	jobs := []int{3, 887, 445, 1013, -45, 6}
+	for _, v := range jobs {
+		go func() {
+			ch <- isEvenNumber(v)
+		}()
+	}
+
+	var evenCount int
+	var oddCount int
+	for evenCount+oddCount < len(jobs) {
+		if <-ch {
+			evenCount++
+		} else {
+			oddCount++
+		}
+	}
+
+	fmt.Printf("yes: %v, no: %v\n", evenCount, oddCount)
+}
